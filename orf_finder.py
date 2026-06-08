094541a7dbe3c9aa0348005777e9929adf691471
Artur <shade0602@yandex.ru>
2026-06-08 15:43:01 +0400

    Update orf_finder.py

--- orf_finder.py
+++ orf_finder.py
@@ -14,9 +14,38 @@ def read_fasta(filename):
     return header, sequence
 
 # Читаем файл
-header, dna = read_fasta("mt_genome.fasta")  # замени на реальное имя
+header, dna = read_fasta("mt_genome.fasta")
 
-print(f"Заголовок: {header[:100]}")
-print(f"Длина ДНК: {len(dna)}")
-print(f"Первые 100 символов: {dna[:100]}")
-print(f"Последние 100 символов: {dna[-100:]}")
+# print(f"Заголовок: {header[:100]}")
+# print(f"Длина ДНК: {len(dna)}")
+# print(f"Первые 100 символов: {dna[:100]}")
+# print(f"Последние 100 символов: {dna[-100:]}")
+
+def find_orfs_in_frame(dna, frame, min_len=300):
+    """Функция для поиска ORF в заданной рамке чтения"""
+    orfs = []
+    i = frame
+    while i < len(dna) - 2:
+        if dna[i:i+3] == "ATG": #ищет первый попавшийся start codon
+            j = i + 3 #делает шаг в 3 нуклеотида от start codon
+            while j < len(dna) - 2: 
+                if dna[j:j+3] in ["TAA", "TAG", "TGA"]: #ищет stop codon
+                    orf_seq = dna[i:j+3]
+                    if len(orf_seq) >= min_len:
+                        orfs.append((i, j+2, orf_seq)) #start index, stop index, последовательность
+                        break
+                j += 3 
+        i += 3
+    return orfs
+
+orfs = find_orfs_in_frame(dna, 0, min_len=300)
+for start, stop, seq in orfs:
+    print(f"ORF_1: {start}-{stop}, длина={len(seq)}, seq={seq}")
+
+orfs = find_orfs_in_frame(dna, 1, min_len=300)
+for start, stop, seq in orfs:
+    print(f"ORF_2: {start}-{stop}, длина={len(seq)}, seq={seq}")
+
+orfs = find_orfs_in_frame(dna, 2, min_len=300)
+for start, stop, seq in orfs:
+    print(f"ORF_3: {start}-{stop}, длина={len(seq)}, seq={seq}")
